23bc4b070b81059161c3efa5913d3fad9b18c807
Maxence Lefort <maxence@hellocarbo.com>
2025-10-08 19:13:39 +0200

    latest updates

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,10 @@
-import TwitterIcon from '@/components/icons/TwitterIcon'
-import GithubIcon from '@/components/icons/GithubIcon'
+import InstagramIcon from '@/components/icons/InstagramIcon'
 
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
 	{
-		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: GithubIcon
-	},
-
-	{
-		name: 'Twitter',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: TwitterIcon
+		name: 'Instagram',
+		url: 'https://instagram.com/terres.sucrees',
+		icon: InstagramIcon
 	}
 ] as const
