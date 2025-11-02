23af6c963bcaeb44de753a7cf11360c4931169bb
Maxence Lefort <maxence@hellocarbo.com>
2025-11-02 23:46:00 +0100

    fixing categories

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,7 +13,7 @@ const blog = defineCollection({
 				.or(z.date())
 				.transform((val) => new Date(val)),
 			heroImage: image(),
-			category: z.array(z.string()),
+			category: z.enum(CATEGORIES),
 			tags: z.array(z.string()),
 			draft: z.boolean().default(false)
 		})
